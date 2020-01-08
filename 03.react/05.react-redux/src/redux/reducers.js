e878f24815d0509ca58a5b99c7a368c5d6717c4f
xiongjian <webjsforyou@163.com>
2020-01-08 10:39:20 +0800

    redux基本使用

---
+++ 03.react/05.react-redux/src/redux/reducers.js
@@ -0,0 +1,21 @@
+/**
+ * 用来根据之前的状态（prevState）和action来生成新状态(newState) 函数模块
+ */
+
+import { INCREMENT, DECREMENT } from './action-types';
+// reducer函数名称一般和要管理的状态数据的名称一致
+// 在reducer中对状态数据进行初始化
+function number(prevState = 0, action) {
+  console.log(prevState, action);
+  
+  switch (action.type) {
+    case INCREMENT:
+      return prevState + action.data;
+    case DECREMENT:
+      return prevState - action.data;
+    default :
+      return prevState;  
+  }
+}
+
+export default number;
